c0d18f61eb89eec5e181bd70794ef1ad42532109
Nathan F Syfrig <nathan@artisticnetwork.com>
2023-08-21 02:45:21 -0700

    Fix map rewriting and copying

--- utils/sourcemap-move.ts
+++ utils/sourcemap-move.ts
@@ -2,12 +2,16 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 const utilsDirectory = 'utils';
-const toRootDirectory = '..';
-const rooToDist = 'dist';
-const rootToDistSrc = 'dist/_src';
+
+const distToRoot = '..';
+const rootToDist = 'dist';
+const distSource = '_src'
+const rootToDistSrc = `dist/${distSource}`;
 const allowedFileExtension = '.map';
 const excluded = [
     'node_modules',
+    'spec',
+    'utils',
     '.git'
 ]
 
@@ -17,7 +21,7 @@ export function moveSourcemap(): void {
         console.error(`This must be run in the ${utilsDirectory} directory`);
         process.exit(1);
     }
-    process.chdir(path.resolve(toRootDirectory));
+    process.chdir(path.resolve(distToRoot));
     const originalDirectory = process.cwd();
 
     console.log(`Syncing sourcemaps:`);
@@ -33,9 +37,10 @@ export function moveSourcemap(): void {
     process.chdir(cwd);
     console.log();
     console.log(`Done`);
+    console.log();
 }
 
-function findMapFiles(directory = rooToDist, fileExtensionFilter = allowedFileExtension): Array<fs.Dirent> {
+function findMapFiles(directory = rootToDist, fileExtensionFilter = allowedFileExtension): Array<fs.Dirent> {
     const newDirContents: Array<fs.Dirent> = [];
     if (!excluded.includes(directory)) {
         const dirContents = fs.readdirSync(`${directory}`, { withFileTypes: true });
@@ -57,65 +62,86 @@ function findMapFiles(directory = rooToDist, fileExtensionFilter = allowedFileEx
     return newDirContents;
 }
 
-function changeMapSourcesPath(mapPath: string, newLocationFromRoot = rootToDistSrc): Object {
+const parentPath = '../';
+function removeParentPaths(source: string): string {
+    let currentSource = source;
+    let parentPathCount = 0;
+
+    while (currentSource.startsWith(parentPath)) {
+        currentSource = currentSource.substring(parentPath.length);
+        ++parentPathCount;
+    }
+
+    return currentSource;
+}
+
+function changeMapSourcesPath(mapPath: string, newSource = distSource): Object {
     const encoding = 'utf-8';
     const contents = fs.readFileSync(mapPath, { encoding: encoding });
     const mapDefinition = JSON.parse(contents);
     const sources = mapDefinition.sources as Array<string> | undefined;
 
-    if (sources) {
-        console.log(`        Map file '${mapPath}':`);
+    if (sources && mapPath.indexOf(newSource) < 0) {
+        console.log(`    Map file '${mapPath}':`);
         for (let sourceIndex = 0; sourceIndex < sources.length; ++sourceIndex) {
             const originalSource = sources[sourceIndex];
-            sources[sourceIndex] = changeSourcePath(sources[sourceIndex], newLocationFromRoot);
-            console.log(`           [original] -> [new]: ${originalSource} -> ${sources[sourceIndex]}`);
+            if (originalSource.indexOf(newSource) < 0) {
+                sources[sourceIndex] = changeSourcePath(sources[sourceIndex], newSource);
+            }
+            console.log(`       [original] -> [new]: ${originalSource} -> ${sources[sourceIndex]}`);
         }
     }
     return mapDefinition;
 
-    function changeSourcePath(source: string, newLocationFromRoot: string): string {
-        const resolvedRoot = path.resolve();
-        const sourceRoot = stripRelativePathComponents(source);
-        const finalContents = `${newLocationFromRoot}/${sourceRoot}`;
+    function changeSourcePath(source: string, newSource: string): string {
+        const sourceWithoutParents = removeParentPaths(source);
+        const parentPathCount = countParentPaths(source);
+        const parentPaths = parentPathCount > 0 ? generateParentPaths(parentPathCount - 1) : '';
+        const finalContents = source.indexOf(newSource) < 0 ? `${parentPaths}${newSource}/${sourceWithoutParents}` : source
 
         return finalContents;
-
-        function stripRelativePathComponents(sourceParam: string): string {
-            let strippedSourceParam = sourceParam;
-            while (strippedSourceParam.startsWith('.')) {
-                strippedSourceParam = movePathToParent(strippedSourceParam);
-            }
-            return strippedSourceParam;
-        }
-        function movePathToParent(sourceParam: string): string {
-            const unixSeparatorIndex = sourceParam.indexOf('/');
-            const dosSeparatorIndex = sourceParam.indexOf('\\');
-            let separatorIndex = 0;
-            if (unixSeparatorIndex > -1 && dosSeparatorIndex > -1) {
-                separatorIndex = unixSeparatorIndex < dosSeparatorIndex ? unixSeparatorIndex : dosSeparatorIndex;
-            } else if (unixSeparatorIndex > -1 || dosSeparatorIndex > -1) {
-                separatorIndex = unixSeparatorIndex > -1 ? unixSeparatorIndex : dosSeparatorIndex;
-            }
-            return sourceParam.substring(separatorIndex + 1);
-        }
     }
+
+    function countParentPaths(source: string): number {
+        let currentSource = source;
+        let parentPathCount = 0;
+    
+        while (currentSource.startsWith(parentPath)) {
+            currentSource = currentSource.substring(parentPath.length);
+            ++parentPathCount;
+        }
+    
+        return parentPathCount;
+    }
+
+    function generateParentPaths(count: number): string {
+        let parentPaths = "";
+        for (let pathIndex = 0; pathIndex < count; ++pathIndex) {
+            parentPaths += parentPath;
+        }
+        return parentPaths;
+    }
+    
 }
 
-function copySources(mapDefinition: any, newLocationFromRoot = rootToDistSrc): void {
+function copySources(mapDefinition: any, distLocation = rootToDist, newSource = distSource): void {
     const sources = mapDefinition.sources as Array<string>;
+
     sources.forEach((source) => {
-        if (source.startsWith(`${newLocationFromRoot}/`)) {
-            const originalSource = source.substring(newLocationFromRoot.length + 1);
-            const originalPath = getOriginalSourcePath(originalSource);
-            fs.mkdirSync(newLocationFromRoot, { recursive: true });
-            if (originalPath.length > 0) {
-                fs.mkdirSync(`${newLocationFromRoot}${path.sep}${originalPath}`, { recursive: true });
-            }
-            fs.copyFileSync(originalSource, source, fs.constants.COPYFILE_FICLONE);
-        }
+        const sourcePath = `${removeParentPathsAndDistSourceAddParentPath(source)}`;
+        const destinationPath = `${newSource}/${removeParentPaths(sourcePath)}`;
+        const distDestinationPath = `${distLocation}/${destinationPath}`;
+        fs.mkdirSync(getPath(distDestinationPath), { recursive: true });
+        fs.copyFileSync(sourcePath, distDestinationPath, fs.constants.COPYFILE_FICLONE);
     });
 
-    function getOriginalSourcePath(sourceParam: string): string {
+    function removeParentPathsAndDistSourceAddParentPath(sourceParam: string): string {
+        const sourceWithoutParents = removeParentPaths(sourceParam);
+        const sourceWithoutSourcePrefixIndex = sourceWithoutParents.indexOf('/');
+        return sourceWithoutParents.substring(sourceWithoutSourcePrefixIndex + 1);
+    }
+
+    function getPath(sourceParam: string): string {
         const lastUnixSeparatorIndex = sourceParam.lastIndexOf('/');
         const lastDosSeparatorIndex = sourceParam.lastIndexOf('\\');
         let lastSeparatorIndex = 0;
